2de4934824fc8c2c0659b8adc70c6339b438e436
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-30 17:40:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const pool = require("pool");
+
+app.use(express.json());
+
+//ROUTES//
+
+//get all todos
+
+// get a todo
+
+// Create a todo
+
+// Update a todo
+
+// Delete a todo
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
