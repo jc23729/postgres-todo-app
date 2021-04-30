4262a9825e8dfda949ede4681d828bd228f883fe
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-30 17:33:35 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,5 +2,22 @@ const Pool = require('pg').Pool;
 
 
 const pool = new Pool({
-    
+
 });
+
+
+/** Database setup other method */
+
+// const { Client } = require("pg");
+
+// const client = new Client({
+//   // connectionString: "postgresql:///biztime"
+//   user: "postgres",
+//   database: "biztime",
+//   host: "localhost",
+//   port: 5432,
+// });
+
+// client.connect();
+
+// module.exports = client;
