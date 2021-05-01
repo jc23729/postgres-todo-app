3d6bda0f48b3a9c5a439f74e482ee653af899bc6
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-01 09:14:51 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,19 @@
 CREATE DATABASE todo_database;
 
-
---\c into todo_database 
-
+--\c into todo_database
 
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
 
-
+--Steps in postgress
+-- 1 command line psql -U postgres
+-- 2 postgres=# \l list all the databases
+-- 3 postgres=# CREATE DATABASE todo_database;
+-- 4 postgres=# \c todo_database    conncects to the databases
+-- 5 we take the create table todo line and past into psql
+--  CREATE TABLE todo(
+--     todo_id SERIAL PRIMARY KEY,
+--     description VARCHAR(255)
+-- );
