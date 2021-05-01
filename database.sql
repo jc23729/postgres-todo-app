c261762f586bc86cb8fda6073546982e2529ec75
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-01 09:51:08 -0400

    Update database.sql
    add psql command notes

--- database.sql
+++ database.sql
@@ -17,3 +17,10 @@ CREATE TABLE todo(
 --     todo_id SERIAL PRIMARY KEY,
 --     description VARCHAR(255)
 -- );
+
+--  psql Common Commands
+-- \l — List all databases
+--  \c DB_NAME — connect to DB_NAME
+--  \dt —- List all tables (in current db)
+--  \d TABLE_NAME — Get details about TABLE_NAME (in current db)
+--  \q — Quit psql (can also type <Control-D>)
