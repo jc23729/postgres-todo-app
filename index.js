688e41d4684ece8c57840f19edc02daf06ab2c02
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-03 22:49:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -218,34 +218,3 @@ app.listen(3000, function () {
 // // Use this to do fast (but not exact) counts from tables. Helpful if table has millions / billions of records and you just want estimated rows quickly. (SQL)
 
 
-// app.use(express.json()); // =>req.body
-
-// //ROUTES//
-
-// //get all todos
-
-// // get a todo
-
-//   // create a todo
-// app.put('/todos', async (req, res) => {
-//   try{
-//     const {description} = req.body;
-//     const newTodo = await pool.query(
-//       "INSERT INTO todo (description) VALUES ($1) RETURNING *",
-//       [description]
-//     );
-//     // //await
-//     // console.log(req.body);
-//     res.json(newTodo.rows[0]);
-
-//   }catch(err){
-//     console.error(err.message);
-//   }
-// });
-// // Update a todo
-
-// // Delete a todo
-
-// app.listen(3000, function () {
-//   console.log('App on port 3000');
-// })
